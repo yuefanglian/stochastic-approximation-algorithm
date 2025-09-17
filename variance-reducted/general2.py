f2324945247223b6cc045c1db10add98b336dbf7
yuefanglian <163313343+yuefanglian@users.noreply.github.com>
2025-09-17 14:43:57 +0800

    Add files via upload
    add the files from the program in DATA(d:) of my computer(named wariance-reducted-2023/9/17)

---
+++ variance-reducted/general2.py
@@ -0,0 +1,245 @@
+import numpy as np
+import math
+import cvxopt
+import matplotlib.pyplot as plt
+from matplotlib import font_manager
+
+
+# the first experiment about the offline QP
+def loadtxtmethod(filename):
+    data = np.loadtxt(filename, dtype=int, delimiter=None, skiprows=2)
+    return data
+
+
+class Submodular_ex:
+    noise = None
+
+    def __init__(self, data):
+        self.p = p
+        self.M = np.zeros((n, n))
+        self.data = data
+
+    def generate(self, data):
+        m = np.array(self.data).shape[0]  # m=数据的行数
+        for i in range(m):
+            self.M[np.array(self.data)[i, 0] - 1][np.array(self.data)[i, 1] - 1] = 100 * np.array(self.data)[i][2]
+        # M[np.array(data)[0, 0] - 1][np.array(data)[0, 1] - 1] = np.array(data)[0][2]
+        # M[np.array(data)[1, 0] - 1][np.array(data)[1, 1] - 1] = np.array(data)[1][2]
+        self.M = (self.M + self.M.T)
+
+    def exact_value_oracle(self, x):
+        y = 0
+        for i in range(n):
+            for j in range(n):
+                if j < i or j > i:
+                    y = y + self.M[i][j] * (1 - (1 - self.p) ** x[i]) * (1 - self.p) ** x[j]
+        return y
+
+    def stochastic_gradient_oracle(self, x):
+        # return a stochastic gradient g=\\\\nable f(x)+\\\\deta*U[-1,1] for E(g)=\\\\nable f(x)=Hx+h\\n\",\n",
+        noise = Submodular_ex.noise * np.random.randn(n, 1)  # 读取矩阵H的行数（shape[0]）,随机生成n*1的（高斯噪声）向量
+        grad = [0] * self.M.shape[0]
+        for t in range(n):
+            for j in range(n):
+                if j < t or j > t:
+                    grad[t] = grad[t] - 2 * self.M[t][j] * (1 - self.p) ** x[j] * (1 - self.p) ** x[t] * math.log(
+                        1 - self.p) + self.M[j][t] * (1 - self.p) ** x[t] * math.log(1 - self.p)
+        return grad + noise
+
+    @staticmethod
+    def set_noise(delta):
+        Submodular_ex.noise = delta  # 给噪声常数
+
+    # @staticmethod
+    # def set_demension(data):
+    #         Submodular_ex.noise = delta  # 给噪声常数
+    #         r = np.array(data).shape[1]
+    #         n = np.max(np.array(data)[:, 0:(r - 1)])
+
+
+# # first method: traditional gradient ascent
+# def projected_gradient_ascent(sub: Submodular_ex, T, batch):
+#     np.random.seed(T)
+#     outcome = [0] * T
+#     point = np.zeros((n, 1))  # 生成n行1列的矩阵
+#     # setting the projection QP as 1/2x^{T}Px+q^{T}x s.t Gx \\\\le g, Cx=c\\n\",\n",
+#     G = cvxopt.matrix(np.concatenate([np.ones((1, n)), -np.ones((1, n)), -np.eye(n)]))  # 拼接三个矩阵的，按行拼接 np.eye(n)生成n维单位向量
+#     g = cvxopt.matrix(np.concatenate([np.array([[1], [-0.25]]), np.zeros((n, 1))]))  # 对应三个约束 Ax\leb,x\le1,-x\le0
+#     P = cvxopt.matrix(2 * np.eye(n))  # 构造目标函数系数，\|x-point\|^{2}~ x^{2}-2point*x
+#     for i in range(T):
+#         gradient = sum(sub.stochastic_gradient_oracle(point) for j in range(batch)) / batch  # 小批量多次采样求平均
+#         point += gradient / math.sqrt(i + 1)  # +=: point=point+gradient...
+#         q = cvxopt.matrix(-2 * point)  # 构造目标函数终一次项系数 -2point
+#         sol = cvxopt.solvers.qp(P, q, G, g)  # 求解二次规划问题：投影步
+#         point = np.array(sol['x'])
+#         point = point.reshape(n, 1)
+#         outcome[i] = sub.exact_value_oracle(point)
+#     return outcome
+
+
+# the method: frankwolfe
+def fw(H, T):
+    # global point1
+    np.random.seed(k)
+    # m = Submodular_QP.constraints.shape[0]
+    # n = Submodular_QP.constraints.shape[1]
+    outcome = [0] * (T)
+    point = np.zeros((n, 1))
+    G = cvxopt.matrix(np.concatenate([np.ones((1, n)), -np.ones((1, n)), -np.eye(n)]))  # 拼接三个矩阵的，按行拼接 np.eye(n)生成n维单位向量
+    g = cvxopt.matrix(np.concatenate([np.array([[1], [-0.25]]), np.zeros((n, 1))]))  # 对应三个约束 Ax\leb,x\le1,-x\le0
+    for i in range(T):
+        outcome[i] = H.exact_value_oracle(point)
+        gradient = H.stochastic_gradient_oracle(point)
+        sol = cvxopt.solvers.lp(cvxopt.matrix(-gradient), G, g)
+        alpha = (math.log(3) / 2) / ((i + 1) * (sum(1 / (t + 1) for t in range(T))))
+        point = (1 - alpha) * point + alpha * np.array(sol['x'])
+    return outcome
+
+
+#
+#
+# #
+# #
+# the method: fw2 of du
+def fw2(H, T=500):
+    np.random.seed(k)
+    # m = Submodular_QP.constraints.shape[0]
+    # n = Submodular_QP.constraints.shape[1]
+    outcome = [0] * (T)
+    point = np.zeros((n, 1))
+    G = cvxopt.matrix(np.concatenate([np.ones((1, n)), -np.ones((1, n)), -np.eye(n)]))  # 拼接三个矩阵的，按行拼接 np.eye(n)生成n维单位向量
+    g = cvxopt.matrix(np.concatenate([np.array([[1], [-0.25]]), np.zeros((n, 1))]))  # 对应三个约束 Ax\leb,x\le1,-x\le0
+    for i in range(T):
+        outcome[i] = H.exact_value_oracle(point)
+        gradient = H.stochastic_gradient_oracle(point)
+        sol = cvxopt.solvers.lp(cvxopt.matrix(-gradient), G, g)
+        alpha = ((1 / T) * (1 + i / T)) / ((1 + (i+1) / T) ** 2)
+        point = (1 - alpha) * point + alpha * np.array(sol['x'])
+    return outcome
+
+
+#
+#
+# our method: SPIDER_SFW
+def spider_stochastic_fw(sub: Submodular_ex, T, batch):
+    np.random.seed(k)
+    # m = Submodular_QP.constraints.shape[0]
+    # n = Submodular_QP.constraints.shape[1]
+    outcome = [0] * (T)
+    point = np.zeros((n, 1))
+    # point[0][0] = 1
+    G = cvxopt.matrix(np.concatenate([np.ones((1, n)), -np.ones((1, n)), -np.eye(n)]))  # 拼接三个矩阵的，按行拼接 np.eye(n)生成n维单位向量
+    g = cvxopt.matrix(np.concatenate([np.array([[1], [-0.25]]), np.zeros((n, 1))]))  # 对应三个约束 Ax\leb,x\le1,-x\le0
+    g_t = sum(sub.stochastic_gradient_oracle(point) for j in range(epoch ** 2)) / (epoch ** 2)
+    for i in range(T):
+        outcome[i] = sub.exact_value_oracle(point)
+        sol = cvxopt.solvers.lp(cvxopt.matrix(-g_t), G, g)
+        point1 = point
+        alpha = ((1 / T) * (1 + i / T)) / ((1 + (i+1) / T) ** 2)
+        point = (1 - alpha) * point + alpha * np.array(sol['x'])
+        g_t = g_t + sum(sub.stochastic_gradient_oracle(point) for j in range(batch)) / batch - sum(
+            sub.stochastic_gradient_oracle(point1) for j in range(batch)) / batch
+    return outcome
+
+
+#
+#
+# our method: Hybrid_SPIDER_SFW
+def hybrid_spider_sfw(H, T, batch):
+    # global point1
+    np.random.seed(k)
+    # m = Submodular_QP.constraints.shape[0]
+    # n = Submodular_QP.constraints.shape[1]
+    outcome = [0] * (T)
+    point = np.zeros((n, 1))
+    G = cvxopt.matrix(np.concatenate([np.ones((1, n)), -np.ones((1, n)), -np.eye(n)]))  # 拼接三个矩阵的，按行拼接 np.eye(n)生成n维单位向量
+    g = cvxopt.matrix(np.concatenate([np.array([[1], [-0.25]]), np.zeros((n, 1))]))  # 对应三个约束 Ax\leb,x\le1,-x\le0
+    g_t = sum(H.stochastic_gradient_oracle(point) for j in range(batch)) / (batch)
+    for i in range(T):
+        outcome[i] = H.exact_value_oracle(point)
+        sol = cvxopt.solvers.lp(cvxopt.matrix(-g_t), G, g)
+        point1 = point
+        alpha = ((1 / T) * (1 + i / T)) / ((1 + (i+1) / T) ** 2)
+        point = (1 - alpha) * point + alpha * np.array(sol['x'])
+        rho = 8 / (i + 8)
+        g_t = sum(H.stochastic_gradient_oracle(point) for j in range(batch)) / batch + (1 - rho) * (g_t - sum(
+            H.stochastic_gradient_oracle(point1) for j in range(batch)) / batch)
+    return outcome
+
+
+if __name__ == "__main__":
+    delta = 1.5
+    data = loadtxtmethod('lesmis.txt')
+    p = 0.0002
+    # print(data)
+    r = np.array(data).shape[1]
+    n = np.max(np.array(data)[:, 0: (r-1)])
+    # M = np.zeros((n, n))
+    # # M = sub_matrix(n)
+    epoch = 200
+    K =10
+    outcome2_K = 0
+    outcome3_K = 0
+    outcome4_K = 0
+    outcome6_K = 0
+    outcome7_K = 0
+
+
+    for k in range(K):
+        sub = Submodular_ex(data)
+        sub.set_noise(delta=delta)
+        sub.generate(data)
+
+        my_font = font_manager.FontProperties(fname=
+                                              "C:/Windows/Fonts/msyh.ttc")
+        plt.xlabel(u'Iteration Index', fontproperties=my_font)
+        plt.ylabel(u'Objective value', fontproperties=my_font)
+
+        # outcome1 = sum(np.array(projected_gradient_ascent(sub, T=epoch, batch=1)) for k in range(K)) / K
+        outcome2 = np.array(spider_stochastic_fw(sub, T=epoch, batch=epoch))
+        outcome3 = np.array(fw(sub, T=epoch))
+        outcome4 = np.array(fw2(sub, T=epoch))
+        # outcome5 = np.array(hybrid_spider_sfw(sub, T=epoch, batch=1))
+        outcome6 = np.array(hybrid_spider_sfw(sub, T=epoch, batch=100))
+        outcome7 = np.array(hybrid_spider_sfw(sub, T=epoch, batch=20))
+
+        my_font = font_manager.FontProperties(fname=
+                                              "C:/Windows/Fonts/msyh.ttc")
+        plt.xlabel(u'Iteration Index', fontproperties=my_font)
+        plt.ylabel(u'Objective value', fontproperties=my_font)
+        # plt.plot(range(len(outcome1)), outcome1, label='PGA')
+        plt.plot(range(len(outcome2)), outcome2, label='spider_FW')
+        # plt.plot(range(len(outcome5)), outcome5, label='hybrid_spider_FW(1)')
+        plt.plot(range(len(outcome6)), outcome6, label='hybrid_spider_FW(100)')
+        plt.plot(range(len(outcome7)), outcome7, label='hybrid_spider_FW(20)')
+        plt.plot(range(len(outcome3)), outcome3, label='non_monotone_fw1')
+        plt.plot(range(len(outcome4)), outcome4, label='non_monotone_fw2')
+        plt.legend()
+        plt.savefig("D:/2023文件/实验图像/20230309/general2/temp{}.png".format(k))  # 输入地址，并利用format函数修改图片名称
+        plt.clf()  # 需要重新更新画布，否则会出现同一张画布上绘制多张图片
+
+        # outcome1 = sum(np.array(projected_gradient_ascent(sub, T=epoch, batch=1)) for k in range(K)) / K
+        outcome2_K = outcome2_K + outcome2
+        outcome3_K = outcome3_K + outcome3
+        outcome4_K = outcome4_K + outcome4
+        outcome6_K = outcome6_K + outcome6
+        outcome7_K = outcome7_K + outcome7
+
+        # outcome3 = sum(np.array(fw(sub, T=epoch)) for k in range(K)) / K
+        # outcome4 = sum(np.array(fw2(sub, T=epoch)) for k in range(K)) / K
+        # outcome5 = sum(np.array(hybrid_spider_sfw(sub, T=epoch, batch=epoch)) for k in range(K)) / K
+
+    my_font = font_manager.FontProperties(fname=
+                                          "C:/Windows/Fonts/msyh.ttc")
+    plt.xlabel(u'Iteration Index', fontproperties=my_font)
+    plt.ylabel(u'Objective value', fontproperties=my_font)
+    # plt.plot(range(len(outcome1)), outcome1, label='PGA')
+    plt.plot(range(len(outcome2_K)), outcome2_K / K, label='spider_FW')
+    plt.plot(range(len(outcome6_K)), outcome6_K / K, label='hybrid_spider_FW(100)')
+    plt.plot(range(len(outcome7_K)), outcome7_K / K, label='hybrid_spider_FW(20)')
+    plt.plot(range(len(outcome3_K)), outcome3_K / K, label='non_monotone_fw1')
+    plt.plot(range(len(outcome4_K)), outcome4_K / K, label='non_monotone_fw2')
+    plt.legend()
+    plt.savefig("D:/2023文件/实验图像/20230309/general2/temp{average}.png")  # 输入地址，并利用format函数修改图片名称
+    plt.show()
+
---
+++ variance-reducted/submodular_nonmonotone.py
@@ -0,0 +1,347 @@
+import numpy as np
+import math
+import cvxopt
+import matplotlib.pyplot as plt
+import matplotlib
+
+
+# the first experiment about the offline QP
+class Submodular_QP:
+    noise = None
+    # we use the Submodular_QP.constraint to record the constraint matrix A, i.e., Ax\\\\le b\n",
+    constraints = None
+
+    # we take QP as f(x)=1/2x^{T}Hx+h^{T}x\
+    def __init__(self):
+        self.H = None
+        self.h = None
+        self.c =None
+
+    def generate(self, n):
+        # n is the dimension of x=(x_{1},...,x_{n})
+        self.H = np.random.rand(n, n)  # 生成随机n维向量，每个元素服从01均匀分布
+        self.H = 0.5 * (self.H + self.H.T)  # 保证H是一个对称矩阵
+        self.H = -self.H  # H的元素均匀分布在[-1，0]上
+        self.h = -0.2 * self.H.dot(np.ones((n, 1))) # 给定h=-H^{T}u,u=(1,...1)^{T},为了保证函数非单调性
+        self.c = float(-0.5* np.ones((n, 1)).T.dot(self.H.dot(np.ones((n, 1)))))
+
+    def stochastic_gradient_oracle(self, x):
+        # return a stochastic gradient g=\\\\nable f(x)+\\\\deta*U[-1,1] for E(g)=\\\\nable f(x)=Hx+h\\n\",\n",
+        noise = Submodular_QP.noise * np.random.randn(self.H.shape[0], 1)  # 读取矩阵H的行数（shape[0]）,随机生成n*1的（高斯噪声）向量
+        return self.H.dot(x) + self.h + noise
+
+    # def harmonic_number(T):
+    #     C = 0
+    #     for j in range(T):
+    #         C = C + 1 / (j+1)
+    #         return C
+
+    def stochastic_boosting_gradient_oracle(self, x):
+        # we first generate a z~Z where Pr(Z\\\\le z)=(exp(z-1)-exp(-1))/(1-exp(-1))\\n\",\n",
+        z = math.log((1 - math.exp(-1)) * np.random.rand() + math.exp(-1)) + 1
+        return (1 - math.exp(-1)) * self.stochastic_gradient_oracle(z * x)
+
+    def exact_value_oracle(self, x):
+        # return f(x)
+        y = float(0.5 * x.T.dot(self.H.dot(x)) + self.h.T.dot(x)+self.c)
+        return y
+
+    @staticmethod
+    def set_constraints(m, n):
+        Submodular_QP.constraints = np.random.rand(m, n)  # 生成m*n的约束矩阵A，每个元素都均匀分布在[0,1]上
+
+    @staticmethod
+    def set_noise(delta):
+        Submodular_QP.noise = delta  # 给噪声常数
+
+    # def set_delay(self, num):
+    #     self.delay = np.random.randint(1, num + 1)
+
+
+# first method: traditional gradient ascent
+def projected_gradient_ascent(sub: Submodular_QP, T=500, batch=1):
+    np.random.seed(k)
+    # m = Submodular_QP.constraints.shape[0]
+    # n = Submodular_QP.constraints.shape[1]
+    m = sub.constraints.shape[0]  # 约束集合行数
+    n = sub.constraints.shape[1]  # 约束集列数
+    outcome = [0] * T
+    point = np.zeros((n, 1))  # 生成n行1列的矩阵
+    # setting the projection QP as 1/2x^{T}Px+q^{T}x s.t Gx \\\\le g, Cx=c\\n\",\n",
+    G = cvxopt.matrix(
+        np.concatenate((Submodular_QP.constraints, np.eye(n), -np.eye(n)), axis=0))  # 拼接三个矩阵的，按行拼接 np.eye(n)生成n维单位向量
+    g = cvxopt.matrix(
+        np.concatenate((np.ones((m, 1)), np.ones((n, 1)), np.zeros((n, 1))), axis=0))  # 对应三个约束 Ax\leb,x\le1,-x\le0
+    P = cvxopt.matrix(2 * np.eye(n))  # 构造目标函数系数，\|x-point\|^{2}~ x^{2}-2point*x
+    for i in range(T):
+        outcome[i] = sub.exact_value_oracle(point)
+        gradient = sum(sub.stochastic_gradient_oracle(point) for j in range(batch)) / batch  # 小批量多次采样求平均
+        point += gradient / math.sqrt(i + 1)  # +=: point=point+gradient...
+        q = cvxopt.matrix(-2 * point)  # 构造目标函数终一次项系数 -2point
+        sol = cvxopt.solvers.qp(P, q, G, g)  # 求解二次规划问题：投影步
+        point = np.array(sol['x'])
+        point = point.reshape(n, 1)
+    return outcome
+
+
+# second method: boosting gradient ascent
+def boosting_gradient_ascent(H, T=500, batch=1):
+    np.random.seed(k)
+    # 种子随机数，每次加上这个代码都可以产生与此次一样的随机数
+    m = Submodular_QP.constraints.shape[0]
+    n = Submodular_QP.constraints.shape[1]
+    outcome = [0] * T
+    point = np.zeros((n, 1))
+    # setting the projection QP as 1/2x^{T}Px+q^{T}x s.t Gx \\\\le g, Cx=c\
+    G = cvxopt.matrix(np.concatenate((Submodular_QP.constraints, np.eye(n), -np.eye(n)), axis=0))
+    g = cvxopt.matrix(np.concatenate((np.ones((m + n, 1)), np.zeros((n, 1))), axis=0))
+    P = cvxopt.matrix(2 * np.eye(n))
+    for i in range(T):
+        outcome[i] = H.exact_value_oracle(point)
+        gradient = sum(H.stochastic_boosting_gradient_oracle(point) for j in range(batch)) / batch
+        point += gradient / math.sqrt(i + 1)
+        q = cvxopt.matrix(-2 * point)
+        sol = cvxopt.solvers.qp(P, q, G, g)
+        point = np.array(sol['x'])
+        point = point.reshape(n, 1)
+    return outcome
+
+#
+# # the third method:conditional gradient ascent
+# def continuous_greedy_method(H, T=500):
+#     np.random.seed(T)
+#     m = Submodular_QP.constraints.shape[0]
+#     n = Submodular_QP.constraints.shape[1]
+#     outcome = [0] * T
+#     point = np.zeros((n, 1))
+#     G = cvxopt.matrix(np.concatenate((Submodular_QP.constraints, np.eye(n), -np.eye(n)), axis=0))
+#     for i in range(T):
+#         outcome[i] = H.exact_value_oracle(point)
+#         g = cvxopt.matrix(np.concatenate((np.ones((m, 1)), (np.ones((n, 1)) - point), np.zeros((n, 1))), axis=0))
+#         gradient = H.stochastic_gradient_oracle(point)
+#         sol = cvxopt.solvers.lp(cvxopt.matrix(-gradient), G, g)  # 针对非单调，需要加约束，刻画约束
+#         point = point + np.array(sol['x']) / T
+#     return outcome
+
+#
+# # the fouth method: SCG
+# def stochastic_greedy_method(H, T=500):
+#     np.random.seed(k)
+#     m = Submodular_QP.constraints.shape[0]
+#     n = Submodular_QP.constraints.shape[1]
+#     outcome = [0] * T
+#     point = np.zeros((n, 1))
+#     G = cvxopt.matrix(np.concatenate((Submodular_QP.constraints, np.eye(n), -np.eye(n)), axis=0))
+#     gradient = H.stochastic_gradient_oracle(point)
+#     for i in range(T):
+#         outcome[i] = H.exact_value_oracle(point)
+#         g = cvxopt.matrix(np.concatenate((np.ones((m, 1)), (np.ones((n, 1)) - point), np.zeros((n, 1))), axis=0))
+#         rho = 2 / ((i + 4) ** (2 / 3))
+#         gradient = (1 - rho) * gradient + rho * H.stochastic_gradient_oracle(point)
+#         sol = cvxopt.solvers.lp(cvxopt.matrix(-gradient), G, g)
+#         point = point + np.array(sol['x']) / T
+#     return outcome
+
+
+# # the final method:SCG++
+# def stochastic_greedy_pp(H, T=500):
+#     np.random.seed(T)
+#     m = Submodular_QP.constraints.shape[0]
+#     n = Submodular_QP.constraints.shape[1]
+#     outcome = [0] * T
+#     point = np.zeros((n, 1))
+#     G = cvxopt.matrix(np.concatenate((Submodular_QP.constraints, np.eye(n), -np.eye(n)), axis=0))
+#     g = cvxopt.matrix(np.concatenate((np.ones((m, 1)), np.ones((n, 1)), np.zeros((n, 1))), axis=0))
+#     g_t = sum(H.stochastic_gradient_oracle(point) for j in range(100 ** 2)) / (100 ** 2)  # 随机梯度
+#     for i in range(T):
+#         gradient = cvxopt.matrix(-g_t)
+#         sol = cvxopt.solvers.lp(gradient, G, g)
+#         point1 = np.array(sol['x']) / T
+#         point = point + point1
+#         outcome[i] = H.exact_value_oracle(point)
+#         g_t = g_t + H.H.dot(point1)
+#         # print(H.H.dot(point1))
+#     return outcome
+
+
+# our method: SPIDER_SFW
+def spider_stochastic_fw(H, T=500, batch=1):
+    np.random.seed(k)
+    m = Submodular_QP.constraints.shape[0]
+    n = Submodular_QP.constraints.shape[1]
+    outcome = [0] * T
+    point = np.zeros((n, 1))
+    G = cvxopt.matrix(np.concatenate((Submodular_QP.constraints, np.eye(n), -np.eye(n)), axis=0))
+    g = cvxopt.matrix(np.concatenate((np.ones((m, 1)), np.ones((n, 1)), np.zeros((n, 1))), axis=0))
+    g_t = sum(H.stochastic_gradient_oracle(point) for j in range(batch** 2)) / (batch ** 2)
+    for i in range(T):
+        outcome[i] = H.exact_value_oracle(point)
+        sol = cvxopt.solvers.lp(cvxopt.matrix(-g_t), G, g)
+        point1 = point
+        alpha = ((1 / T) * (1 + i / T)) / ((1 + (i+1) / T) ** 2)
+        point = (1 - alpha) * point + alpha * np.array(sol['x'])
+        g_t = g_t + sum(H.stochastic_gradient_oracle(point) for j in range(batch)) + sum(
+            H.stochastic_gradient_oracle(point1) for j in range(batch))
+    return outcome
+
+
+# our method: Hybrid_SPIDER_SFW
+def hybrid_spider_sfw(H, T=500, batch=1):
+    # global point1
+    np.random.seed(k)
+    m = Submodular_QP.constraints.shape[0]
+    n = Submodular_QP.constraints.shape[1]
+    outcome = [0] * T
+    point = np.zeros((n, 1))
+    G = cvxopt.matrix(np.concatenate((Submodular_QP.constraints, np.eye(n), -np.eye(n)), axis=0))
+    g = cvxopt.matrix(np.concatenate((np.ones((m, 1)), np.ones((n, 1)), np.zeros((n, 1))), axis=0))
+    g_t = sum(H.stochastic_gradient_oracle(point) for j in range(batch ** 2)) / (batch ** 2)
+    for i in range(T):
+        outcome[i] = H.exact_value_oracle(point)
+        sol = cvxopt.solvers.lp(cvxopt.matrix(-g_t), G, g)
+        point1 = point
+        alpha = ((1 / T) * (1 + i / T)) / ((1 + (i+1) / T) ** 2)
+        point = (1 - alpha) * point + alpha * np.array(sol['x'])
+        rho = (i + 1) ** (-1)
+        g_t = sum(H.stochastic_gradient_oracle(point) for j in range(batch)) + (1 - rho) * (g_t - sum(
+            H.stochastic_gradient_oracle(point1) for j in range(batch)))
+    return outcome
+
+
+# the method: frankwolfe
+def fw(H, T=500):
+    # global point1
+    np.random.seed(k)
+    m = Submodular_QP.constraints.shape[0]
+    n = Submodular_QP.constraints.shape[1]
+    outcome = [0] * T
+    point = np.zeros((n, 1))
+    G = cvxopt.matrix(np.concatenate((Submodular_QP.constraints, np.eye(n), -np.eye(n)), axis=0))
+    g = cvxopt.matrix(np.concatenate((np.ones((m, 1)), np.ones((n, 1)), np.zeros((n, 1))), axis=0))
+    for i in range(T):
+        outcome[i] = H.exact_value_oracle(point)
+        gradient = H.stochastic_gradient_oracle(point)
+        sol = cvxopt.solvers.lp(cvxopt.matrix(-gradient), G, g)
+        alpha = (math.log(3) / 2) / ((i + 1) * (sum(1 / (t + 1) for t in range(T))))
+        point = (1 - alpha) * point + alpha * np.array(sol['x'])
+    return outcome
+
+
+# the method: fw2 of du
+def fw2(H, T=500, batch=1):
+    np.random.seed(k)
+    m = Submodular_QP.constraints.shape[0]
+    n = Submodular_QP.constraints.shape[1]
+    outcome = [0] * T
+    point = np.zeros((n, 1))
+    G = cvxopt.matrix(np.concatenate((Submodular_QP.constraints, np.eye(n), -np.eye(n)), axis=0))
+    g = cvxopt.matrix(np.concatenate((np.ones((m, 1)), np.ones((n, 1)), np.zeros((n, 1))), axis=0))
+    for i in range(T):
+        outcome[i] = H.exact_value_oracle(point)
+        gradient = H.stochastic_gradient_oracle(point)
+        sol = cvxopt.solvers.lp(cvxopt.matrix(-gradient), G, g)
+        alpha = ((1 / T) * (1 + i / T)) / ((1 + (i+1) / T) ** 2)
+        point = (1 - alpha) * point + alpha * np.array(sol['x'])
+    return outcome
+# the method: fw2 of du
+def fw3(H, T=500, batch=1):
+    np.random.seed(k)
+    m = Submodular_QP.constraints.shape[0]
+    n = Submodular_QP.constraints.shape[1]
+    outcome = [0] * T
+    point = np.zeros((n, 1))
+    G = cvxopt.matrix(np.concatenate((Submodular_QP.constraints, np.eye(n), -np.eye(n)), axis=0))
+    g = cvxopt.matrix(np.concatenate((np.ones((m, 1)), np.ones((n, 1)), np.zeros((n, 1))), axis=0))
+    for i in range(T):
+        outcome[i] = H.exact_value_oracle(point)
+        gradient = H.stochastic_gradient_oracle(point)
+        sol = cvxopt.solvers.lp(cvxopt.matrix(-gradient), G, g)
+        alpha = 1-2**(-1/T)
+        point = (1 - alpha) * point + alpha * np.array(sol['x'])
+    return outcome
+
+
+if __name__ == '__main__':
+    delta = 10
+    constrain_m = 15
+    dimension = 25
+    # delay_num = 5
+    epoch = 200
+    K = 20
+    outcome1_K = 0
+    outcome6_K = 0
+    outcome7_K = 0
+    outcome8_K = 0
+    outcome11_K = 0
+    outcome9_K = 0
+    outcome10_K = 0
+    outcome12_K = 0
+
+    for k in range(K):
+        sub = Submodular_QP()
+        sub.set_noise(delta=delta)
+        sub.set_constraints(m=constrain_m, n=dimension)
+        # sub.set_delay(delay_num)
+        sub.generate(dimension)
+        #
+        # outcome1 = np.array(projected_gradient_ascent(sub, T=epoch))
+        # outcome2 = continuous_greedy_method(sub, T=epoch)
+        # outcome3 = stochastic_greedy_method(sub, T=epoch)
+        # outcome4 = stochastic_greedy_pp(sub, T=epoch)
+        # outcome5 = boosting_gradient_ascent(sub, T=epoch, batch=1)
+        # outcome6 = np.array(boosting_gradient_ascent(sub, T=epoch, batch=100))
+        outcome7 = np.array(spider_stochastic_fw(sub, T=epoch, batch=epoch))
+        outcome8 = np.array(hybrid_spider_sfw(sub, T=epoch, batch=100))
+        outcome11 = np.array(hybrid_spider_sfw(sub, T=epoch, batch=10))
+        outcome9 = np.array(fw(sub, T=epoch))
+        outcome10 = np.array(fw2(sub, T=epoch))
+        # outcome12 = np.array(fw3(sub, T=epoch))
+
+        # my_font = font_manager.FontProperties(fname=
+        #                                       "C:/Windows/Fonts/msyh.ttc")
+        plt.xlabel('Iteration Index')
+        plt.ylabel('Objective value')
+        # plt.plot(range(len(outcome1)), outcome1, label='PGA')
+        # plt.plot(range(len(outcome2)), outcome2, label='CG')
+        # plt.plot(range(len(outcome3)), outcome3, label='SGA')
+        # plt.plot(range(len(outcome4)), outcome4, label='SG++')
+        # plt.plot(range(len(outcome5)), outcome5, label='BGA(1)')
+        # plt.plot(range(len(outcome6)), outcome6, label='BGA(100)')
+        plt.plot(range(len(outcome7)), outcome7, label='spider_FW')
+        plt.plot(range(len(outcome8)), outcome8, label='hybrid_spider_FW(100)')
+        plt.plot(range(len(outcome11)), outcome11, label='hybrid_spider_FW(10)')
+        plt.plot(range(len(outcome9)), outcome9, label='non_monotone_fw1')
+        plt.plot(range(len(outcome10)), outcome10, label='non_monotone_fw2')
+        # plt.plot(range(len(outcome12)), outcome12, label='non_monotone_fw3')
+
+        plt.legend()
+        plt.savefig("D:/2023文件/实验图像/20230702/non-monotone-qp/temp{}.png".format(k))  # 输入地址，并利用format函数修改图片名称
+        plt.clf()  # 需要重新更新画布，否则会出现同一张画布上绘制多张图片
+
+        # outcome1_K = outcome1_K + outcome1
+        # outcome6_K = outcome6_K + outcome6
+        outcome7_K = outcome7_K + outcome7
+        outcome8_K = outcome8_K + outcome8
+        outcome11_K = outcome11_K + outcome11
+        outcome9_K = outcome9_K + outcome9
+        outcome10_K = outcome10_K + outcome10
+        # outcome12_K = outcome12_K + outcome12
+
+    # my_font = font_manager.FontProperties(fname=
+    #                                       "C:/Windows/Fonts/msyh.ttc")
+    plt.xlabel('Iteration Index')
+    plt.ylabel('Objective value')
+    # plt.plot(range(len(outcome1_K)), outcome1_K / K, label='PGA')
+    # plt.plot(range(len(outcome2)), outcome2, label='CG')
+    # plt.plot(range(len(outcome3)), outcome3, label='SGA')
+    # plt.plot(range(len(outcome4)), outcome4, label='SG++')
+    # plt.plot(range(len(outcome5)), outcome5, label='BGA(1)')
+    # plt.plot(range(len(outcome6_K)), outcome6_K / K, label='BGA(100)')
+    plt.plot(range(len(outcome7_K)), outcome7_K / K, label='spider_FW')
+    plt.plot(range(len(outcome8_K)), outcome8_K / K, label='hybrid_spider_FW(100)')
+    plt.plot(range(len(outcome11_K)), outcome11_K / K, label='hybrid_spider_FW(10)')
+    plt.plot(range(len(outcome9_K)), outcome9_K / K, label='non_monotone_fw1')
+    plt.plot(range(len(outcome10_K)), outcome10_K / K, label='non_monotone_fw2')
+    # plt.plot(range(len(outcome12_K)), outcome12_K / K, label='non_monotone_fw3')
+    plt.legend()
+    plt.savefig("D:/2023文件/实验图像/20230702/non-monotone-qp/temp{average}.png")  # 输入地址，并利用format函数修改图片名称
